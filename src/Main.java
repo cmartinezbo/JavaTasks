5bde916e4b12a07126eb701e032f97c8d395382b
ericmp <ericmorenopou@gmail.com>
2021-08-10 20:38:49 +0200

    todo

--- src/Main.java
+++ src/Main.java
@@ -7,3 +7,13 @@ public class Main {
     }
 }
 
+/*
+todo: Inicializacion(): Inicializa las variables relacionada al array ó list tipo Persona.
+
+todo: AgregarPersona(Persona persona): recibe como parámetro un objeto tipo persona y lo adiciona al array ó list tipo persona.
+
+todo: ListarPersona(): debe recorrer el array ó list tipo Persona y mostrar los registros con el toString().
+
+done: ProcesarComando(): debe poder realizar la lectura desde consola con la siguiente estructura:
+ -> opción&tipoPersona&nombre&identificación&teléfono&dirección&elemento
+ */
