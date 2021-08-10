ab3a5eb775b086eaf9e4a5ba8b24f9a5abc62c85
CrisMB17 <cristianmartinez1700@gmail.com>
2021-08-10 12:47:07 -0500

    Options v1

--- src/Gestion.java
+++ src/Gestion.java
@@ -1,7 +1,15 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Gestion {
     public static void main(String[] args) {
-        System.out.println("test");
+        String string = "1&Proveedor&Carlos&123456&55555&avenida siempre viva 45&llantas1";
+        String[] parts = string.split("&");
+        if (parts[0].equals("1")) {
+            // Añadir persona
+        } else if (parts[0] .equals("2")) {
+            // Imprimir datos persona
+        } else {
+            // Salir
+        }
     }
 }
