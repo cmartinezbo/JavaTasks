a2a8d4ee4d384c8477c62de17c8c9f90fb6abf10
ericmp <ericmorenopou@gmail.com>
2021-08-10 20:49:44 +0200

    arraylist

--- src/Gestion.java
+++ src/Gestion.java
@@ -1,21 +1,26 @@
 import entidades.Persona;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Gestion {
-    // Inicializa las variables relacionada al array ó list tipo entidades.Persona
-    private void inicializacion() {
+    private ArrayList<Persona> personas;
 
+    // Inicializa las variables relacionada al array ó list tipo entidades.Persona
+    private void Gestion() {
+        personas = new ArrayList<>();
     }
 
     // recibe como parámetro un objeto tipo persona y lo adiciona al array ó list tipo persona.
     private void agregarPersona(Persona persona) {
-
+        personas.add(persona);
     }
 
     // debe recorrer el array ó list tipo entidades.Persona y mostrar los registros con el toString()
     private void listarPersona() {
-
+        for (int i = 0; i < personas.size(); i++) {
+            System.out.println(personas.get(i).toString());
+        }
     }
 
     // debe poder realizar la lectura desde consola con la siguiente estructura:
