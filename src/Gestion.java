084af236543368de70c10b52448f23b5b41b2e51
CrisMB17 <cristianmartinez1700@gmail.com>
2021-08-11 11:44:00 -0500

    Final Version

--- src/Gestion.java
+++ src/Gestion.java
@@ -1,9 +1,9 @@
 import entidades.Persona;
-import entidades.Proveedor;
-import entidades.Socio;
+        import entidades.Proveedor;
+        import entidades.Socio;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+        import java.util.ArrayList;
+        import java.util.Scanner;
 
 public class Gestion {
     // attributes
@@ -60,5 +60,41 @@ public class Gestion {
     public static void main(String[] args) {
         var miGestion = new Gestion();
         miGestion.procesarComando();
+
+        /* ENTRADA:
+
+        1&Proveedor&Carlos&123456&55555&avenida siempre viva 45&llantas
+
+        1&Socio&Manuel&789456&777777&calle 10 N 4-25&23
+
+        2
+
+        3 */
+
+        /* SALIDA:
+
+        ***Inventario de Personas***
+
+                Persona Proveedor - Nombre: Carlos
+
+                Identificación: 123456
+
+                Teléfono: 55555
+
+                Dirección: avenida siempre viva 45
+
+                Producto: llantas
+
+                Persona Socio - Nombre: Manuel
+
+                Identificación: 789456
+
+                Teléfono: 777777
+
+                Dirección: calle 10 N 4-25
+
+                Antigüedad: 23 años
+
+         */
     }
 }
