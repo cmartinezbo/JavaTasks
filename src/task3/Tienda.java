e6fed8843955375ca76b787739567376566215a5
CrisMB17 <cristianmartinez1700@gmail.com>
2021-08-19 19:05:10 -0500

    Added new packages

---
+++ src/task3/Tienda.java
@@ -0,0 +1,12 @@
+package task3;
+
+import java.util.ArrayList;
+
+public class Tienda {
+    public static void met_filtrar_productos() {
+        ArrayList<String> myList = new ArrayList<String>();
+
+
+
+    }
+}
