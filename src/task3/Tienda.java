af36c175a6fff73ab457bdaaa097d259e6d17244
CrisMB17 <cristianmartinez1700@gmail.com>
2021-08-23 21:41:03 -0500

    task finished

--- src/task3/Tienda.java
+++ src/task3/Tienda.java
@@ -1,28 +1,23 @@
 package task3;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 
 public class Tienda {
 
     // Primer método
-    public static LinkedHashSet<String> met_filtrar_productos(String str) {
-        ArrayList<String> inputList = new ArrayList<>(Arrays.asList(str.trim().split(", ")));
-
-        return new LinkedHashSet<>(inputList);
+    public static LinkedHashSet<String> met_filtrar_productos(ArrayList<String> str) {
+        return new LinkedHashSet<>(str);
     }
 
     // Segundo método
-    public static ArrayList<String> met_productos_faltantes(String posiciones, String faltantes, String elemento) {
+    public static ArrayList<Integer> met_productos_faltantes(ArrayList<Integer> posiciones, ArrayList<String> faltantes, String elemento) {
 
-        ArrayList<String> firstList = new ArrayList<>(Arrays.asList(posiciones.trim().split(",")));
-        ArrayList<String> secondList = new ArrayList<>(Arrays.asList(faltantes.trim().split(", ")));
-        ArrayList<String> output = new ArrayList<>();
+        ArrayList<Integer> output = new ArrayList<>();
 
-        for (String item : firstList) {
-            if (secondList.get(Integer.parseInt(item)).equals(elemento)) {
+        for (Integer item : posiciones) {
+            if (faltantes.get(item).equals(elemento)) {
                 output.add(item);
             }
         }
@@ -30,14 +25,12 @@ public class Tienda {
     }
 
     // Tercer método
-    public static ArrayList<String> met_encontrar_faltantes(String pedido, String producto) {
+    public static ArrayList<String> met_encontrar_faltantes(ArrayList<String> pedido, ArrayList<String> producto) {
 
-        ArrayList<String> firstList = new ArrayList<>(Arrays.asList(pedido.trim().split(", ")));
-        ArrayList<String> secondList = new ArrayList<>(Arrays.asList(producto.trim().split(", ")));
         ArrayList<String> output = new ArrayList<>();
 
-        for (String item : firstList) {
-            if (!secondList.contains(item)) {
+        for (String item : pedido) {
+            if (!producto.contains(item)) {
                 output.add(item);
             }
         }
@@ -45,39 +38,22 @@ public class Tienda {
     }
 
     // Cuarto método
-    public static int met_obtener_intercambiables(String otros, String mios) {
+    public static String met_obtener_intercambiables(ArrayList<String> otros, ArrayList<String> mios) {
 
-        ArrayList<String> firstList = new ArrayList<>(Arrays.asList(otros.trim().split(", ")));
-        ArrayList<String> secondList = new ArrayList<>(Arrays.asList(mios.trim().split(", ")));
         ArrayList<String> firstChanger = new ArrayList<>();
         ArrayList<String> secondChanger = new ArrayList<>();
 
-        for (String item : firstList) {
-            if (!secondList.contains(item)) {
+        for (String item : otros) {
+            if (!mios.contains(item)) {
                 firstChanger.add(item);
             }
         }
 
-        for (String item : secondList) {
-            if (!firstList.contains(item)) {
+        for (String item : mios) {
+            if (!otros.contains(item)) {
                 secondChanger.add(item);
             }
         }
-        return Math.min(firstChanger.size(), secondChanger.size());
-    }
-
-    // Main - Pruebas
-    public static void main(String[] args) {
-        System.out.println(met_filtrar_productos("arroz, sal, miel, lentejas, frijol, leche, lentejas, arroz, " +
-                "miel, sal, banano"));
-
-        System.out.println(met_productos_faltantes("0,3,4,2,1,5,6,8,11", "arroz, sal, miel, " +
-                "lentejas, frijol, leche, lentejas, arroz, miel, sal, banano, arroz", "arroz" ));
-
-        System.out.println(met_encontrar_faltantes("arroz, sal, miel, lentejas, banano, frijol",
-                "sal, miel, ajo, frijol, leche"));
-
-        System.out.println(met_obtener_intercambiables("arroz, sal, miel, lentejas, banano, frijol, papa, " +
-                "cebolla, aceite", "sal, miel, ajo, frijol, leche, garbanzo, lechuga, papa"));
+        return Integer.toString(Math.min(firstChanger.size(), secondChanger.size()));
     }
 }
