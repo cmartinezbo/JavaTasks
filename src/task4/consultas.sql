9f3ade3e8692cd3255105cc0f22fec06aac606ae
Cristian Martínez <cristianmartinez1700@gmail.com>
2021-09-01 11:25:49 -0500

    Update consultas.sql

--- src/task4/consultas.sql
+++ src/task4/consultas.sql
@@ -9,4 +9,4 @@ SELECT * FROM factura WHERE ven_nombre_vendedor = 'Jesús Daniel';
 SELECT 'Consulta 5';
 SELECT cli_id_fk, ven_nro_factura_fk, cli_id, cli_nombre, cli_apellido, cli_direccion, cli_telefono, cli_fecha_nacimiento FROM compra, cliente WHERE compra.id_compra = cliente.cli_id order by cli_id;
 SELECT 'Consulta 6';
-SELECT count(cli_id_fk) FROM compra GROUP BY cli_id_fk;
+SELECT  concat(cliente.cli_nombre,' ', cliente.cli_apellido), count(compra.cli_id_fk) FROM cliente INNER JOIN compra ON compra.cli_id_fk = cliente.cli_id GROUP BY cli_id_fk ORDER BY cli_id_fk DESC;
