4a1b4c3f644017daf87af537c3e91adb4879e962
Cristian Martínez <cristianmartinez1700@gmail.com>
2021-09-02 12:33:34 -0500

    Delete actualizaciones.sql

--- src/task4/actualizaciones.sql
+++
@@ -1,3 +0,0 @@
-UPDATE proveedor SET prov_nombre = 'la superior ltda' WHERE prov_codigo = 126;
-UPDATE cliente SET cli_telefono = 5556565 WHERE cli_id = 4;
-DELETE FROM proveedor where prov_codigo = 130;
