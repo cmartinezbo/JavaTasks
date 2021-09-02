808136557854712ceb12ada36e9ba4639e573ee1
Cristian Martínez <cristianmartinez1700@gmail.com>
2021-09-02 12:33:25 -0500

    Update consultas.sql

--- src/task4/consultas.sql
+++ src/task4/consultas.sql
@@ -1,3 +1,6 @@
+UPDATE proveedor SET prov_nombre = 'la superior ltda' WHERE prov_codigo = 126;
+UPDATE cliente SET cli_telefono = 5556565 WHERE cli_id = 4;
+DELETE FROM proveedor where prov_codigo = 130;
 SELECT 'Consulta 1';
 SELECT * FROM proveedor;
 SELECT 'Consulta 2';	
@@ -7,6 +10,6 @@ SELECT * FROM producto WHERE prod_valor > 10000;
 SELECT 'Consulta 4';
 SELECT * FROM factura WHERE ven_nombre_vendedor = 'Jesús Daniel';
 SELECT 'Consulta 5';
-SELECT cli_id_fk, ven_nro_factura_fk, cli_id, cli_nombre, cli_apellido, cli_direccion, cli_telefono, cli_fecha_nacimiento FROM compra, cliente WHERE compra.id_compra = cliente.cli_id order by cli_id;
+SELECT cli_id_fk, ven_nro_factura_fk, cli_id, cli_nombre, cli_apellido, cli_direccion, cli_telefono, cli_fecha_nacimiento FROM cliente INNER JOIN compra ON compra.cli_id_fk = cliente.cli_id order by cliente.cli_id ASC;
 SELECT 'Consulta 6';
-SELECT  concat(cliente.cli_nombre,' ', cliente.cli_apellido), count(compra.cli_id_fk) FROM cliente INNER JOIN compra ON compra.cli_id_fk = cliente.cli_id GROUP BY cli_id_fk ORDER BY cli_id_fk DESC;
+SELECT cliente.cli_nombre || ' ' || cliente.cli_apellido, count(compra.cli_id_fk) FROM cliente INNER JOIN compra ON compra.cli_id_fk = cliente.cli_id GROUP BY cli_id_fk ORDER BY count(compra.cli_id_fk), cliente.cli_nombre DESC;		
