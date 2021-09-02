cf4431a75e186c0bc75958fc6a61a0f79b521447
Cristian Martínez <cristianmartinez1700@gmail.com>
2021-09-02 12:32:19 -0500

    Update factura.sql

--- src/task4/factura.sql
+++ src/task4/factura.sql
@@ -1,22 +1,22 @@
 CREATE TABLE factura( 
-	ven_nro_factura int AUTO_INCREMENT, 
+	ven_nro_factura int NOT NULL, 
 	ven_valor_compra int NOT NULL, 
 	ven_fecha_compra date NOT NULL, 
 	ven_sucursal varchar(30) NOT NULL, 
 	ven_nombre_vendedor varchar(40) NULL,
 	PRIMARY KEY(ven_nro_factura)
-)AUTO_INCREMENT = 7001;
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (251320, '2021-02-12', 'las maletas', 'Jesús Daniel');
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (51350, '2021-02-13', 'las nubes', 'Pedro Banco');
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (23000, '2021-02-14', 'las maletas', 'Oscar Marx');
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (120000, '2021-02-15', 'las ciudadela', 'Petrona Daniel');
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (231000, '2021-02-16', 'las maletas', 'Jesús Daniel');
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (19000, '2021-02-17', 'las nubes', 'Claudio Rodri');
-INSERT INTO factura(ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
-VALUES (25000, '2021-02-18', 'las maletas', 'Jesús Daniel');
+);
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7001, 251320, '2021-02-12', 'las maletas', 'Jesús Daniel');
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7002, 51350, '2021-02-13', 'las nubes', 'Pedro Banco');
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7003, 23000, '2021-02-14', 'las maletas', 'Oscar Marx');
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7004, 120000, '2021-02-15', 'las ciudadela', 'Petrona Daniel');
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7005, 231000, '2021-02-16', 'las maletas', 'Jesús Daniel');
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7006, 19000, '2021-02-17', 'las nubes', 'Claudio Rodri');
+INSERT INTO factura(ven_nro_factura, ven_valor_compra, ven_fecha_compra, ven_sucursal, ven_nombre_vendedor) 
+VALUES (7007, 25000, '2021-02-18', 'las maletas', 'Jesús Daniel');
